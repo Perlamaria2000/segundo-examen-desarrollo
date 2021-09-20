9c5361f1904ca43455cb1038f63f02166b560593
unknown <mcisnerosmazariegis@gmail.com>
2021-09-20 13:04:16 -0600

    kkkk

--- configurations/db-conf.js
+++ configurations/db-conf.js
@@ -1,7 +1,7 @@
-const host="";
-const user="";
-const password="";
-const database="";
+const host="nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+const user="r53rpt8nw5afs1a3";
+const password="s9lfyl2qe0n5apre";
+const database="mrd9nkrribyj0oas";
 
 exports.host=host;
 exports.user= user;
